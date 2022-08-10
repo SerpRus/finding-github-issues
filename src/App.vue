dcf1f46fea9f6d6cca792469f516886af0a0f06c
SerpRus <Slawka00@yandex.ru>
2022-08-10 10:46:50 +0300

    Start project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  App
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
